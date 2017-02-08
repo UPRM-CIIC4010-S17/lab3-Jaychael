33532cb519e34c311c7c89dc34dd334f5d02ce27
4010c4 <4010c4@melon.ece.uprm.edu>
2017-02-08 11:54:41 -0400

    Changed name

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -2,10 +2,10 @@ import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Hello, world!");
-		//myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation(200, 200);
-		myFrame.setSize(400, 100);
-		myFrame.setVisible(true);
+		JFrame jaychael = new JFrame("Hello, world!");
+		jaychael.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		jaychael.setLocation(200, 200);
+		jaychael.setSize(200, 200);
+		jaychael.setVisible(true);
 	}
 }
